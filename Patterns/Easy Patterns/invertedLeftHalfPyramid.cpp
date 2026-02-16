12638b74bfe78e71db1f6fe9e8c53d5a023aabe3
pragyata03 <pragyata03s@gmail.com>
2026-02-16 14:10:44 +0530

    modified

---
+++ Patterns/Easy Patterns/invertedLeftHalfPyramid.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter the value of n: "<<endl;
+    cin>>n;
+    
+    for(int i=0; i<=n; i++){
+        for(int j=0; j<=i-1; j++){
+            cout<<" ";
+        }
+        for(int j=0; j<n-i+1; j++){
+            cout<<"*";
+        }
+        
+        cout<<endl;
+    }
+    return 0;
+}
+
+
+/*
+Enter the value of n: 
+5
+******
+ *****
+  ****
+   ***
+    **
+     *
+*/
